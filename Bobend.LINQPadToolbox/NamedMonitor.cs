04ac4ddc7a3668345c9546d979ff0b521deae163
Bo Bendtsen <bben@dr.dk>
2019-03-14 12:54:06 +0100

    Added NamedMonitor.

---
+++ Bobend.LINQPadToolbox/NamedMonitor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+
+namespace Bobend.LINQPadToolbox
+{
+    public class NamedMonitor
+    {
+        private readonly ConcurrentDictionary<string, object> _dictionary = new ConcurrentDictionary<string, object>();
+
+        public object this[string name] => _dictionary.GetOrAdd(name, _ => new object());
+    }
+}
